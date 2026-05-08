42c3b2ac36b76b18a39cfc9ee36c9627d4d21ca7
rahad-hossen <mdrahadhossen811@gmail.com>
2026-05-08 23:14:42 +0600

    Initial commit - NextStore start

---
+++ android/app/src/main/java/com/rhrahadtec/nextstore/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rhrahadtec.nextstore;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:nextstore/Screens/HomeScreens/Home.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
